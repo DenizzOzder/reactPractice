cf800023fe9e7837d46da4b5f07ebd4183b08a68
Deniz Can Özder <candenizozder08@gmail.com>
2026-01-13 14:18:09 +0300

    feat(ProductDetails): Add Add To Cart button Set ustate to setCount Block when count go to below 0

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getAllProducts } from "../../redux/productSlice";
@@ -14,7 +14,18 @@ export default function ProductDetails() {
       dispatch(getAllProducts());
     }
   }, [dispatch, products]);
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   const selectedItem = products.find((item) => item.id === parseInt(id));
+  const { category, image, price, title, description } = selectedItem || {};
 
   if (loading) {
     return <Loading />;
@@ -23,11 +34,19 @@ export default function ProductDetails() {
   return (
     <ul>
       <li key={selectedItem.id}>
-        <p className="Category">{selectedItem.category}</p>
-        <img src={selectedItem.image} alt={selectedItem.title} width="50" />
-        <p>Price: {selectedItem.price}₺</p>
-        <p>{selectedItem.title}</p>
-        <p>{selectedItem.description}</p>
+        <p className="Category">{category}</p>
+        <img src={image} alt={title} width="50" />
+        <p>Price: {price}₺</p>
+        <p>{title}</p>
+        <p>{description}</p>
+        <div>
+          <p>
+            <span onClick={decrement}> - </span>
+            {count}
+            <span onClick={increment}> + </span>
+          </p>
+          <button>Add to Cart</button>
+        </div>
       </li>
     </ul>
   );
