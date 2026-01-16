22b185811f35fbece110c11a11cb557521498ded
Deniz <candenizozder08@gmail.com>
2026-01-16 16:10:24 +0300

    feat(finished): Finished project with Add & Delete specs. Installed dependecies redux & redux toolkit Created Redux folder, store.ts, slices folder and todoSlice.tsx addTodo & deleteTodo defined at todoSlice.tsx Created Form.tsx & List.tsx component Add Input & button & List in Form component

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,34 @@
+import "./Form.css"
+import List from '../List/List.tsx'
+import { useState } from "react"
+import { addTodo, deleteTodo } from "../../redux/slices/todoSlice.tsx"
+import { useAppDispatch, useAppSelector } from "../../redux/Hook.ts"
+export default function Form() {
+
+  const [text,setText] = useState('');
+  const dispatch = useAppDispatch();
+
+  const handleAdd = () => {
+    if(!text.trim()) return;
+    //Yeni Todo 
+    const newTodo = {
+      id:crypto.randomUUID(),
+      text:text
+    };
+    dispatch(addTodo(newTodo));
+    setText('')
+  }
+  return (
+    <div>
+        <div className="inputArea">
+            <input type="text" 
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              placeholder="Eklenecek Todo"
+            />
+            <button onClick={handleAdd}>To Do Ekle</button>
+        </div>
+        <List />
+    </div>
+  )
+}
