62dc889380be90d79c90158c76692bf3978c1f89
Deniz <candenizozder08@gmail.com>
2026-01-19 19:04:20 +0300

    Add Card & Skeleton component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import AutoComplete from './component/AutoComplete'
 import Buttons from './component/Buttons'
 import Checkboxs from './component/Chekboxs'
+import  Card  from './component/Card'
+import Skeleton from './component/Skeleton'
 function App() {
 
   return (
@@ -11,6 +13,10 @@ function App() {
       <Buttons />
       <hr />
       <Checkboxs />
+      <hr />
+      <Card />
+      <hr />
+      <Skeleton />
     </>
   )
 }
