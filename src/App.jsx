2f6b0a41ada9eb84bdce44bced3db89b86b7e86e
Deniz Can Özder <candenizozder08@gmail.com>
2026-01-21 15:57:41 +0300

    Add Register function Created a user succesfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
 import RouterConfig from "./config/routerConfig";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <>
       <Header />
       <RouterConfig />
+      {/* Toast Bildirimleri -- 3 Saniye sonra oto yok olcak */}
+      <ToastContainer position="top-right" autoClose={3000} />
     </>
   );
 }
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,20 +1,50 @@
 import { Button } from "@mui/material";
 import React from "react";
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { toast, ToastContainer } from "react-toastify";
+import { auth } from "../Firebase";
 
 export default function Auth() {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  const Register = async () => {
+    try {
+      const response = await createUserWithEmailAndPassword(auth, email, pass);
+      const user = response.user;
+      if (user) {
+        toast.success("Kullanıcı Oluşturuldu");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
   return (
     <>
       <div className="wrapper">
         Auth
         <div className="form">
           <label htmlFor="username">Kullanıcı adı</label>
-          <input type="text" id="username" />
+          <input
+            type="text"
+            id="username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label htmlFor="password">Şifre</label>
-          <input type="password" id="password" />
+          <input
+            type="password"
+            id="password"
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+          />
         </div>
         <div className="buttonWrapper">
           <Button variant="contained">Giriş Yap</Button>
-          <Button variant="contained">Kayıt Ol</Button>
+          <Button variant="contained" onClick={Register}>
+            Kayıt Ol
+          </Button>
           <Button variant="contained">Google ile Giriş</Button>
         </div>
       </div>
